baf824eac80413ff2a6e7caebe9f8dd23638cd56
John Wildkins <john.wildkins@gmail.com>
2023-10-21 21:21:35 -0400

    homogenize messages to factory module

--- Plugins/Public/base_plugin/FactoryModule.cpp
+++ Plugins/Public/base_plugin/FactoryModule.cpp
@@ -64,9 +64,10 @@ wstring FactoryModule::GetInfo(bool xml)
 			if (gi)
 			{
 				info += openLine + L"- " + stows(itos(quantity)) + L"x " + HkGetWStringFromIDS(gi->iIDSName);
-				if (quantity > 0 && base->HasMarketItem(good) < active_recipe.cooking_rate)
+				uint has_quantity = base->HasMarketItem(good);
+				if (has_quantity < quantity)
 				{
-					info += L" [Out of stock]";
+					info += L" [Only " + UIntToPrettyStr(has_quantity) + L" units available]";
 				}
 			}
 		}
